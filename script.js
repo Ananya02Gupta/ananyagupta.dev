808d0c2dba883841f71e2ebd5f16fd38b39d2f61
Ananya02Gupta <ananyagupta315@gmail.com>
2026-01-20 18:49:02 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,10 @@
+<script>
+  const navLinks = document.querySelectorAll(".navbar a");
+
+  navLinks.forEach(link = {
+    link.addEventListener("click", function () {
+      navLinks.forEach(l => l.classList.remove("active"));
+      this.classList.add("active");
+    })
+  });
+</script>
